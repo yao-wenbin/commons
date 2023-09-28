caecff59d4e2712788fcf04398e277114790f786
yao-wenbin <ywb992134@163.com>
2023-09-28 15:03:54 +0800

    feat file: write 、read and createAPI

--- src/main/java/io/github/yaowenbin/commons/file/Files.java
+++ src/main/java/io/github/yaowenbin/commons/file/Files.java
@@ -1,48 +1,59 @@
 package io.github.yaowenbin.commons.file;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.OpenOption;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Files {
 
-    public static void string2File(String str, File file) throws IOException {
-        string2File(str, file.getAbsolutePath());
+    public static void write(File file, String str) throws IOException {
+        createIfNotExist(file);
+
+        try (OutputStream os = java.nio.file.Files.newOutputStream(file.toPath());){
+            os.write(str.getBytes(StandardCharsets.UTF_8));
+        }
     }
 
-    public static void string2File(String str, String filename) throws IOException {
-        File file = new File(filename);
-        File fileParent = file.getParentFile();
-        if (fileParent != null) {
-            fileParent.mkdirs();
-        }
+    /**
+     * overwrite file of path's content with str.
+     * @param path file path.
+     * @param content content need to write in file.
+     */
+    public static void write(Path path, String content) throws IOException {
+        write(path.toFile(), content);
+    }
 
-        OutputStream os = null;
-        try {
-            os = java.nio.file.Files.newOutputStream(file.toPath());
-            os.write(str.getBytes(StandardCharsets.UTF_8));
-        } finally {
-            if (null != os) {
-                os.close();
-            }
+    public static String read(Path path) throws IOException {
+        try (BufferedReader reader = newBufferedReader(path)) {
+            return reader.lines().collect(Collectors.joining());
         }
+    }
+
+    public static String read(File file) throws IOException {
+        return read(file.toPath());
     }
 
     public static InputStream newInputStream(Path path, OpenOption... options) throws IOException {
         return java.nio.file.Files.newInputStream(path, options);
     }
 
-    public static OutputStream newOutPutStream(Path path, OpenOption... options) throws IOException {
-        return java.nio.file.Files.newOutputStream(path, options);
+    public static BufferedReader newBufferedReader(Path path, OpenOption... options) throws IOException {
+        Reader reader = new InputStreamReader(newInputStream(path));
+        return new BufferedReader(reader);
     }
 
-    public static Path createFile(Path filePath) throws IOException {
-        return java.nio.file.Files.createFile(filePath);
+    public static OutputStream newOutPutStream(Path path, OpenOption... options) throws IOException {
+        return java.nio.file.Files.newOutputStream(path, options);
     }
 
     public static Path writeString(Path path, String s) throws IOException {
@@ -60,4 +71,48 @@ public class Files {
         Arrays.asList(arr);
         return fileWatcher;
     }
+
+    /**
+     * make sure path's parent path exist.
+     * if parent path not exist, this method will create path for it.
+     * @param path any path in File system.
+     */
+    public static void makeSureParentExist(Path path) {
+        File parent = path.toFile().getParentFile();
+        if (parent == null || parent.exists()) {
+            return;
+        }
+        parent.mkdirs();
+    }
+
+
+    public static Path create(Path filePath) throws IOException {
+        return java.nio.file.Files.createFile(filePath);
+    }
+
+    public static Path createIfNotExist(Path path) throws IOException {
+        if (exists(path)) {
+            return path;
+        }
+        makeSureParentExist(path);
+
+        return java.nio.file.Files.createFile(path);
+    }
+
+    public static Path createIfNotExist(String strPath) throws IOException{
+        Path path = Paths.get(strPath);
+        return createIfNotExist(path);
+    }
+
+    public static File createIfNotExist(File file) throws IOException{
+        Path path = file.toPath();
+        Path existPah = createIfNotExist(path);
+        return existPah.toFile();
+    }
+
+    public static boolean exists(Path path) {
+        return path != null && path.toFile().exists();
+    }
+
+
 }
--- src/test/java/io/github/yaowenbin/commons/file/FilesTest.java
+++ src/test/java/io/github/yaowenbin/commons/file/FilesTest.java
@@ -2,10 +2,12 @@ package io.github.yaowenbin.commons.file;
 
 import io.github.yaowenbin.commons.string.Strings;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
@@ -23,21 +25,104 @@ public class FilesTest {
 
     @BeforeEach
     void setUp() throws IOException {
-        filePath = Files.createFile(tmpDir.resolve("common.txt"));
+        filePath = Files.create(tmpDir.resolve("common.txt"));
     }
 
     @Test
     void writeString() throws IOException {
-        Files.writeString(filePath, "initial");
-        InputStream inputStream = Files.newInputStream(filePath);
 
-        byte[] bytes = new byte[inputStream.available()];
-        inputStream.read(bytes);
-        String res = new String(bytes);
+    }
+
+    @Test
+    void write() throws IOException {
+        Files.write(filePath, "initial");
+
+        String res = testRead(filePath.toString());
 
         assertThat(res).isEqualTo("initial");
     }
 
+    @Test
+    void write_shouldOverrideFileContent() throws IOException {
+        Files.write(filePath, "initial");
+        Files.write(filePath, "second initial");
+
+        String res = testRead(filePath.toString());
+
+        assertThat(res).isEqualTo("second initial");
+    }
+
+    @Test
+    void read() throws IOException {
+        Files.write(filePath, "Hello World");
+        String res = Files.read(filePath);
+        assertThat(res).isEqualTo("Hello World");
+    }
+
+    @Test
+    void read_multiLines() throws IOException {
+        String complexString = "Hello World \n Foo Bar \t Yes";
+
+        Files.write(filePath, complexString);
+        String res = Files.read(filePath);
+        assertThat(res).isEqualTo("Hello World  Foo Bar \t Yes");
+    }
+
+    @Test
+    void exists_path() throws IOException {
+        boolean res = Files.exists(null);
+        assertThat(res).isFalse();
+
+        Path randonPath = tmpDir.resolve("askdlfjskljfklas");
+        res = Files.exists(randonPath);
+        assertThat(res).isFalse();
+
+        Path existsPath = Files.createIfNotExist(randonPath);
+        res = Files.exists(existsPath);
+        assertThat(res).isTrue();
+    }
+
+    @Test
+    void makeSureParentExist() {
+        Path multiUnExistsPath = tmpDir.resolve("notExistsDirLevel1").resolve("notExistsDirLevel2").resolve("notExistsDirLevel3").resolve("notExistsFile");
+
+        Files.makeSureParentExist(multiUnExistsPath);
+        boolean parentExists = multiUnExistsPath.getParent().toFile().exists();
+        assertThat(parentExists).isTrue();
+    }
+
+    @Test
+    void createIfNotExist_withPath() throws IOException {
+        Path unexistPath = tmpDir.resolve("notExistsDirLevel1").resolve("notExistsDirLevel2").resolve("ksadjflksajfklsajflksajldf.txt");
+        System.out.println(unexistPath);
+
+        Path existPath = Files.createIfNotExist(unexistPath);
+
+        Path res = Files.createIfNotExist(existPath);
+        assertThat(res.toFile().exists()).isTrue();
+    }
+
+    @Test
+    void createIfNotExist_withString() throws IOException {
+        String unexistPath = tmpDir.resolve("ksadjflksajfklsajflksajldf.txt").toString();
+
+        Path existPath = Files.createIfNotExist(unexistPath);
+        assertThat(existPath.toFile().exists()).isTrue();
+
+        Path res = Files.createIfNotExist(existPath);
+        assertThat(res.toFile().exists()).isTrue();
+        assertThat(res.toString()).isEqualTo(unexistPath);
+    }
+
+    @Test
+    void createIfNotExist_withFile() throws IOException {
+        File unexistsFile = tmpDir.resolve("ksadjflksajfklsajflksajldfsadjfklasjdf.txt").toFile();
+
+        File existsFile = Files.createIfNotExist(unexistsFile);
+
+        assertThat(existsFile.exists()).isTrue();
+        assertThat(existsFile.toString()).isEqualTo(unexistsFile.toString());
+    }
 
     @Test
     void watch() throws IOException, InterruptedException {
@@ -57,7 +142,6 @@ public class FilesTest {
 
 
     @Test
-    @Disabled
     void watchStop() {
         FileWatcher fileWatcher = Files.watch(filePath, (filePath) -> {
             System.out.println(Strings.format("file:{} changed", filePath));
@@ -65,6 +149,13 @@ public class FilesTest {
         fileWatcher.shutdown();
 
         assertThat(fileWatcher.stopped()).isTrue();
+    }
+
+    private String testRead(String file) throws IOException {
+        InputStream is = new BufferedInputStream(new FileInputStream(file));
+        byte[] bytes = new byte[is.available()];
+        is.read(bytes);
+        return new String(bytes);
     }
 
 }
