11f4f9bb2b8f9ae094a4028fcf004da318c89429
yao-wenbin <ywb992134@163.com>
2023-11-23 10:09:50 +0800

    feat: add some ruoyi's usefule utils

---
+++ src/main/java/io/github/yaowenbin/commons/constant/Constants.java
@@ -0,0 +1,34 @@
+package io.github.yaowenbin.commons.constant;
+
+public class Constants {
+
+    private Constants() {}
+
+    /**
+     * UTF-8 字符集
+     */
+    public static final String UTF8 = "UTF-8";
+
+    /**
+     * GBK 字符集
+     */
+    public static final String GBK = "GBK";
+
+    /**
+     * www主域
+     */
+    public static final String WWW = "www.";
+
+    /**
+     * http请求
+     */
+    public static final String HTTP = "http://";
+
+    /**
+     * https请求
+     */
+    public static final String HTTPS = "https://";
+
+
+
+}
